f6a4080064971a87a6739dbef74c252f0ac46403
Ivakhnenkoalex <Ivakhnenko_alex@bk.ru>
2020-09-24 12:28:24 +0300

    ЧЕтвертый

--- ft_rev_num.py
+++ ft_rev_num.py
@@ -3,18 +3,9 @@ def ft_rew_num(a):
     k = 0
     if a == 0:
         return a
-    elif a > 0:
-        while a > 0:
-            k = a % 10
-            a = a // 10
-            p = p * 10
-            p = p + k
-        return p
-    elif a < 0:
-        a = -a
-        while a > 0:
-            k = a % 10
-            a = a // 10
-            p = p * 10
-            p = p + k
-        return p
+    while a > 0:
+        k = a % 10
+        a = a // 10
+        p = p * 10
+        p = p + k
+    return p
